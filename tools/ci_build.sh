56ef500cd502cee71047765e69fd94d17b040968
Debojeet Das <debojeetofficial@gmail.com>
2025-03-26 13:27:21 +0530

    fix: updated ci script

--- tools/ci_build.sh
+++ tools/ci_build.sh
@@ -8,11 +8,12 @@ if [ $(id -u) -ne 0 ]; then
     SUDO=sudo
 fi
 
+$SUDO apt update
 $SUDO apt install -y build-essential meson libbpf-dev pkg-config git gcc-multilib clang llvm lld m4 libpcap-dev libcjson-dev libncurses-dev
 
 git clone https://github.com/xdp-project/xdp-tools.git
-make -C xdp-tools
-$SUDO make -C xdp-tools install
+make -j -C xdp-tools libxdp
+$SUDO make -j -C xdp-tools libxdp_install
 
 meson setup build
 meson compile -C build
