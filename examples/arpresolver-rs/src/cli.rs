9d6ebed3a68dc2cf7602f12583dd34631f2ba743
Arghyadip Chakraborty <dev@arghyac.com>
2025-07-11 03:33:49 +0800

    feat(rust): added arpresolver-rs

---
+++ examples/arpresolver-rs/src/cli.rs
@@ -0,0 +1,28 @@
+use clap::Parser;
+use flash::FlashConfig;
+use macaddr::MacAddr6;
+
+#[derive(Debug, Parser)]
+pub struct Cli {
+    #[command(flatten)]
+    pub flash_config: FlashConfig,
+
+    #[arg(
+        short = 'c',
+        long,
+        default_value_t = 0,
+        help = "Starting CPU core index for socket threads"
+    )]
+    pub cpu_start: usize,
+
+    #[arg(
+        short = 'e',
+        long,
+        default_value_t = 0,
+        help = "Ending CPU core index for socket threads (inclusive)"
+    )]
+    pub cpu_end: usize,
+
+    #[arg(short = 'm', long, help = "Interface MAC address")]
+    pub mac_addr: MacAddr6,
+}
---
+++ examples/arpresolver-rs/src/main.rs
@@ -0,0 +1,103 @@
+mod cli;
+mod nf;
+
+use std::{
+    net::Ipv4Addr,
+    sync::{
+        Arc,
+        atomic::{AtomicBool, Ordering},
+    },
+    thread,
+};
+
+use clap::Parser;
+use flash::Socket;
+use macaddr::MacAddr6;
+
+use crate::cli::Cli;
+
+fn socket_thread(mut socket: Socket, mac_addr: MacAddr6, ip_addr: Ipv4Addr, run: &Arc<AtomicBool>) {
+    while run.load(Ordering::SeqCst) {
+        if !socket.poll().is_ok_and(|val| val) {
+            continue;
+        }
+
+        let Ok(descs) = socket.recv() else {
+            continue;
+        };
+
+        let (descs_send, descs_drop) = descs.into_iter().partition(|desc| {
+            socket
+                .read_exact(desc)
+                .is_ok_and(|pkt| nf::arp_resolve(pkt, mac_addr, ip_addr))
+        });
+
+        socket.send(descs_send);
+        socket.drop(descs_drop);
+    }
+}
+
+fn main() {
+    #[cfg(feature = "tracing")]
+    tracing_subscriber::fmt::init();
+
+    let cli = Cli::parse();
+
+    let (sockets, route) = match flash::connect(&cli.flash_config) {
+        Ok(t) => t,
+        Err(err) => {
+            eprintln!("{err}");
+            return;
+        }
+    };
+
+    if sockets.is_empty() {
+        eprintln!("no sockets received");
+        return;
+    }
+
+    #[cfg(feature = "tracing")]
+    tracing::debug!("Sockets: {:?}", sockets);
+
+    let cores = core_affinity::get_core_ids()
+        .unwrap_or_default()
+        .into_iter()
+        .filter(|core_id| core_id.id >= cli.cpu_start && core_id.id <= cli.cpu_end)
+        .collect::<Vec<_>>();
+
+    if cores.is_empty() {
+        eprintln!("No cores found in range {}-{}", cli.cpu_start, cli.cpu_end);
+        return;
+    }
+
+    #[cfg(feature = "tracing")]
+    tracing::debug!("Cores: {:?}", cores);
+
+    let run = Arc::new(AtomicBool::new(true));
+
+    let r = run.clone();
+    if let Err(err) = ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+    }) {
+        eprintln!("error setting Ctrl-C handler: {err}");
+        return;
+    }
+
+    let handles = sockets
+        .into_iter()
+        .zip(cores.into_iter().cycle())
+        .map(|(socket, core_id)| {
+            let r = run.clone();
+            thread::spawn(move || {
+                core_affinity::set_for_current(core_id);
+                socket_thread(socket, cli.mac_addr, route.ip_addr, &r);
+            })
+        })
+        .collect::<Vec<_>>();
+
+    for handle in handles {
+        if let Err(err) = handle.join() {
+            eprintln!("error in thread: {err:?}");
+        }
+    }
+}
